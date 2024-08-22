e281b1ad5c1e3f7fc217a31bb5221fd7acf2620a
Ilya <32960658+IlyaAleichik@users.noreply.github.com>
2024-08-22 15:06:46 +0300

    Update script-ubuntu-deploy.sh

--- script-ubuntu-deploy.sh
+++ script-ubuntu-deploy.sh
@@ -1,11 +1,11 @@
 #REPOSITORY DEFINE
-sudo add-apt-repository ppa:agornostal/ulauncher 
-sudo add-apt-repository ppa:appimagelauncher-team/stable
+#sudo add-apt-repository ppa:agornostal/ulauncher 
+#sudo add-apt-repository ppa:appimagelauncher-team/stable
 
 #REMOVE SNAP
-sudo systemctl stop snapd && sudo systemctl disable snapd
-sudo apt purge snapd
-rm -rf ~/snapsudo rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd /root/snap
+#sudo systemctl stop snapd && sudo systemctl disable snapd
+#sudo apt purge snapd
+#rm -rf ~/snapsudo rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd /root/snap
 
 #APT
 echo APT Apps Install  ...\n
@@ -14,10 +14,10 @@ sudo apt install ulauncher git unrar appimagelauncher dconf-editor alacarte
 ##GIT SETTINGS
 git config --global user.name IlyaAleichik  
 git config --global user.email ilya.alejchik@outlook.com
-git clone git@github.com:vinceliuice/vimix-gtk-themes.git
-sudo ./vimix-gtk-themes/install.sh -c dark -t doder -s compact
-git clone git@github.com:vinceliuice/WhiteSur-icon-theme.git
-sudo ./WhiteSur-icon-theme/install.sh
+#git clone git@github.com:vinceliuice/vimix-gtk-themes.git
+#sudo ./vimix-gtk-themes/install.sh -c dark -t doder -s compact
+#git clone git@github.com:vinceliuice/WhiteSur-icon-theme.git
+#sudo ./WhiteSur-icon-theme/install.sh
 
 ##DOTNET
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb && \
@@ -38,21 +38,24 @@ dotnet new --install ./
 sudo apt-get install -y mssql-server && 
 sudo /opt/mssql/bin/mssql-conf setup && 
 systemctl status mssql-server --no-pager &&
+##PSQL
+##MYSQL
+##SQLITE
 
 ###PATH
-sudo cp -f /patch/crack/winewrapper.exe.so /opt/cxoffice/lib/wine/ &&
-sudo cp -f ./patch/bitwig.jar /opt/bitwig-studio/bin/ &&
+#sudo cp -f /patch/crack/winewrapper.exe.so /opt/cxoffice/lib/wine/ &&
+#sudo cp -f ./patch/bitwig.jar /opt/bitwig-studio/bin/ &&
 
 ##FIX
 sudo timedatectl set-local-rtc 1 --adjust-system-clock ##fix localtime linux
 sudo apt --fix-broken install
 
 #DRIVERS AND LIBS
-sudo apt install --reinstall linux-generic
-sudo apt install lib32z1
-sudo apt install ubuntu-restricted-extras
-sudo apt install xserver-xorg-input-synaptics
-sudo ubuntu-drivers autoinstall
+#sudo apt install --reinstall linux-generic
+#sudo apt install lib32z1
+#sudo apt install ubuntu-restricted-extras
+#sudo apt install xserver-xorg-input-synaptics
+#sudo ubuntu-drivers autoinstall
 
 echo "Application sucessfull installed" || "Instalation filed"
 
