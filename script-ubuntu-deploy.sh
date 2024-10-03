4866310e7e50369f4ed15ec38d71eedf53601615
Ilya <32960658+IlyaAleichik@users.noreply.github.com>
2024-10-03 15:31:30 +0300

    Update script-ubuntu-deploy.sh

--- script-ubuntu-deploy.sh
+++ script-ubuntu-deploy.sh
@@ -39,6 +39,18 @@ sudo apt-get install -y mssql-server &&
 sudo /opt/mssql/bin/mssql-conf setup && 
 systemctl status mssql-server --no-pager &&
 
+
+##PSQL
+sudo apt install curl ca-certificates
+sudo install -d /usr/share/postgresql-common/pgdg
+sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+sudo sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt update
+sudo apt -y install postgresql
+sudo -u postgresql psql
+alter user postgresql with password 'password';
+create user ilya with password 'password';
+
 ###PATH
 sudo cp -f /patch/crack/winewrapper.exe.so /opt/cxoffice/lib/wine/ &&
 sudo cp -f ./patch/bitwig.jar /opt/bitwig-studio/bin/ &&
